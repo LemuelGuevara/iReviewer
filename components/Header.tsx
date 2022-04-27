27e7f6adc6597bd29ea1bf8dc006456dd7c44ca4
RiderxL <lemuelkuya@gmail.com>
2022-04-28 02:15:58 +0800

    Adding Banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,27 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import iReviewer from '../public/iReviewer.png'
 
 function Header() {
   return (
-    <header className="flex h-20 justify-between bg-[#F6F7F1] p-6 font-poppins font-semibold text-base">
-      <div className="flex items-center cursor-pointer">
-        <Link href="/">
-          <img
-            src='https://github.com/RiderxL/iReviewer/blob/main/components/public/iReviewer.png'
-            alt='iReviewer'
-          />
-        </Link>
-        <div className="pl-20 hidden items-center space-x-5 text-base text-[#34A0A3] md:inline-flex">
+    <header className="flex justify-between mx-6 p-6 max-h-20 font-normal text-sm">
+      <div className="flex items-center space-x-6">
+        <div className='h-24 w-44 object-contain relative'>
+          <Link href="/">
+            <Image 
+            className='cursor-pointer'
+            src="/iReviewer-Logo.png"  
+            layout='fill'/>
+          </Link>
+        </div>
+        
+        <div className="hidden md:inline-flex items-center space-x-6 text-[#8bb6b6]">
           <h3>Explore</h3>
           <h3>Trending</h3>
           <h3>Rewards</h3>
         </div>
       </div>
-
-
-      <div className="flex items-center space-x-4 text-[#A1C9C9]">
+      
+      <div className="flex items-center space-x-5 text-[#A1C9C9]">
         <h3 className="">Sign In</h3>
         <h3 className="rounded-lg bg-[#DC9488] px-4 py-1 text-white">
           Sign Up
@@ -31,3 +32,4 @@ function Header() {
 }
 
 export default Header
+
