369b76a8c52ff8226fc06170c7239febb49a73f4
RiderxL <lemuelkuya@gmail.com>
2022-04-30 02:48:32 +0800

    Adding Banner

---
+++ components/Banner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+function Banner() {
+  return (
+    <div>
+        <div>
+            <h1>iReviewer</h1>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
